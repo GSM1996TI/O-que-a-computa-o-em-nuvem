7ed218c45b31a94a10e9259a816e7eda06d242c4
GSM1996TI <gabrielsantanati@outlook.com>
2025-06-18 11:45:47 -0300

    Add files via upload

--- O que é a computação em nuvem?/assets/js.js
+++ O que é a computação em nuvem?/assets/js.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    function setupButton(buttonId, targetUrl) {
-        const button = document.getElementById(buttonId);
-        if (button) {
-            button.addEventListener('click', function() {
-                window.location.href = targetUrl;
-            });
-        }
-    }
-    setupButton('btnIaaS', './IaaS.html');
-    setupButton('btnPaaS', './PaaS.html');
-    setupButton('btnSaaS', './SaaS.html');
-    setupButton('btnVoltarInicio', './index.html'); 
-    setupButton('btnContato', './contato_html.html'); 
-});
+//removido e transferido para index.html
